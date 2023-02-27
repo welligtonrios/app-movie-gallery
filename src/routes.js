a0edfeb05ea773ce0f42313612e30b800c6e5a9d
welligton.rios <welligtonrios2@gmail.com>
2023-02-27 08:16:40 -0300

    feat: add header component

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,12 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Filme from './pages/Filme';
 
+import Header from './components/Header';
+
 const RoutesApp = () => {
     return (
         <BrowserRouter>
+        <Header/>
             <Routes>
                 <Route path='/' element={<Home/>}/>
                 <Route path='/Filme/:id' element={<Filme/>}/>
