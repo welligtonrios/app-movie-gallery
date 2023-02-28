fc01dc3e465804a0973adfa07eb447d10d57da61
welligton.rios <welligtonrios2@gmail.com>
2023-02-28 07:34:40 -0300

    feat: add service to list movie

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,24 @@
+import { useEffect, useState } from "react";
+import movies_db_service from '../../services/moviesDbService';
+import {PRIVATE_KEY_MOVIES_DB} from '../../services/Constants/constants'
+
 const Home = () => {
+
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(()=>{
+        async function loadFilmes(){
+            const response = await movies_db_service.get("movie/now_playing",{
+                params:{
+                    api_key: PRIVATE_KEY_MOVIES_DB,
+                    language: "us-US",
+                    page:1
+                }
+            });
+        }
+        loadFilmes();
+    })
+
     return(
         <div>
             <h1>Home</h1>
---
+++ src/services/Constants/constants.js
@@ -0,0 +1,6 @@
+const PRIVATE_KEY_MOVIES_DB = "8d76b08ee2fea8dda3fb9b6a73de7924";
+const BASE_URL_MOVIES_DB = "https://api.themoviedb.org/3/";
+
+
+
+export {PRIVATE_KEY_MOVIES_DB, BASE_URL_MOVIES_DB}
