aa058717a39b144b752bb8e5d0d0a33e012f2941
welligton.rios <welligtonrios2@gmail.com>
2023-03-09 07:51:42 -0300

    feat: add react toastfy in project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import RoutesApp from "./routes";
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className="App">
+      <ToastContainer autoClose={30000}/>
       <RoutesApp/>
     </div>
   );
